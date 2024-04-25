7b74d2e2d67e812ff3b7559f343555769ee4691f
csirak1528 <calebsirak@gmail.com>
2024-04-24 18:13:56 -0700

    fixed stupid git stuff

--- src/main/scala/com/maj/parser/Parser.scala
+++ src/main/scala/com/maj/parser/Parser.scala
@@ -1,24 +1,40 @@
 package com.maj.parser
 
+import com.maj.ast.ASTNode
+
 import scala.annotation.tailrec
 
-case class ParseResult[+T](value: T, source: Source)
+case class ParseResult[+T](val value: T, val source: Source)
 
 class Parser[+T](val parse: Source => Option[ParseResult[T]]) {
   def apply(): Parser[T] = this
 
-  def or[U >: T](parser: Parser[U]): Parser[U] = new Parser(source => parse(source).orElse(parser.parse(source)))
+  def or[U >: T](parser: Parser[U]): Parser[U] = {
+    new Parser(source => {
+      val result = parse(source);
+      result.orElse(parser.parse(source))
+    })
+  }
 
-  def and[U](parser: Parser[U]): Parser[U] = bind(_ => parser())
+  def and[U](parser: Parser[U]): Parser[U] = {
+    bind(_ => parser())
+  }
 
-  def map[U](callback: T => U): Parser[U] = bind(value => Parser.constant(callback(value)))
+  def map[U](callback: (T) => U): Parser[U] = {
+    bind((value) => {
+      Parser.constant(callback(value))
+    })
+  }
 
-  def bind[U](callback: T => Parser[U]): Parser[U] = new Parser[U](source =>
-    parse(source) match {
-      case Some(ParseResult(value, nextSource)) => callback(value).parse(nextSource)
-      case None => None
-    }
-  )
+  def bind[U](callback: (T) => Parser[U]): Parser[U] = {
+    new Parser[U](source => {
+      val result = parse(source)
+      result match {
+        case Some(ParseResult(value, nextSource)) => callback(value).parse(nextSource)
+        case _ => None
+      }
+    })
+  }
 
   def optional[U >: T]: Parser[Option[U]] = new Parser(source =>
     parse(source) match {
@@ -29,16 +45,33 @@ class Parser[+T](val parse: Source => Option[ParseResult[T]]) {
 
   def flatMap[U]: (T => Parser[U]) => Parser[U] = bind[U]
 
-  private def parseToCompletion(string: String, log: Boolean = false): T = {
+  def parseToCompletion(string: String, log: Boolean = false): T = {
     val source = Source(string, 0, log)
     parse(source) match {
-      case Some(ParseResult(value, remainingSource)) =>
-        if (remainingSource.atEnd()) value
-        else throw new Exception(s"Parse error: Incomplete input at index ${remainingSource.index}\nRemaining input: ${remainingSource}")
+      case Some(ParseResult(value, remainingSource)) => if (remainingSource.atEnd()) value else throw new Exception(s"Parse error: Incomplete input at index ${remainingSource.index}\n Remaining input: ${remainingSource}")
       case None => throw new Exception("Parse error: Unable to parse input")
     }
   }
-<<<<<<< Updated upstream
+
+  def zeroOrMore: Parser[List[T]] = new Parser(source => {
+    @tailrec
+    def recursiveParse(source: Source, acc: List[T]): Option[ParseResult[List[T]]] = {
+      this.parse(source) match {
+        case Some(ParseResult(value, nextSource)) => recursiveParse(nextSource, acc :+ value)
+        case None => Some(ParseResult(acc, source))
+      }
+    }
+
+    recursiveParse(source, List.empty)
+  })
+
+  def parseFile(path: String): ASTNode = {
+    val sourceFile = scala.io.Source.fromFile(path, "utf-8")
+    val lines = sourceFile.getLines.mkString("\n")
+    val ast = Token.parser.parseToCompletion(lines)
+    sourceFile.close()
+    ast
+  }
 }
 
 
@@ -56,44 +89,14 @@ object Parser {
   def zeroOrMore[U](parser: Parser[U]): Parser[List[U]] = {
     @tailrec def recursiveParse(source: Source, acc: List[U]): ParseResult[List[U]] = {
       parser.parse(source) match {
-=======
-
-  def zeroOrMore: Parser[List[T]] = new Parser(source => {
-    @tailrec
-    def recursiveParse(source: Source, acc: List[T]): Option[ParseResult[List[T]]] = {
-      this.parse(source) match {
->>>>>>> Stashed changes
         case Some(ParseResult(value, nextSource)) => recursiveParse(nextSource, acc :+ value)
-        case None => Some(ParseResult(acc, source))
+        case None => ParseResult(acc, source)
       }
     }
 
-    recursiveParse(source, List.empty)
-  })
-
-  def parseFile(path: String, log: Boolean = false): ASTNode = {
-    val sourceFile = scala.io.Source.fromFile(path, "utf-8")
-    val lines = sourceFile.getLines.mkString("\n")
-    val ast = Token.parser.parseToCompletion(lines, log)
-    sourceFile.close()
-    ast
+    new Parser(source => Some(recursiveParse(source, List.empty)))
   }
 }
 
-object Parser {
-  def constant[U](value: U): Parser[U] = new Parser(source => Some(ParseResult(value, source)))
 
-  def error[U](message: String): Parser[U] = new Parser(_ => throw new Exception(message))
 
-  def zeroOrMore[U](parser: Parser[U]): Parser[List[U]] = new Parser(source => {
-    @tailrec
-    def recursiveParse(source: Source, acc: List[U]): Option[ParseResult[List[U]]] = {
-      parser.parse(source) match {
-        case Some(ParseResult(value, nextSource)) => recursiveParse(nextSource, acc :+ value)
-        case None => Some(ParseResult(acc, source))
-      }
-    }
-
-    recursiveParse(source, List.empty)
-  })
-}
